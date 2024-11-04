74df6eab07c09d0aebb63b8f56e37d7f0575ed63
Alson Garbuja <magar33alson@gmail.com>
2024-11-04 12:59:21 +0545

    action: add chopping and collecting action to player and test scenes

--- scenes/characters/player/chopping_state_script.gd
+++ scenes/characters/player/chopping_state_script.gd
@@ -2,6 +2,11 @@ extends NodeState
 
 @export var player: Player
 @export var player_animated_sprite_2d: AnimatedSprite2D
+@export var hit_component_collision_shape: CollisionShape2D
+
+func _ready():
+	hit_component_collision_shape.disabled = true
+	hit_component_collision_shape.position = Vector2(0, 0)
 
 func _on_process(_delta : float) -> void:
 	pass
@@ -15,15 +20,24 @@ func _on_next_transitions() -> void:
 
 func _on_enter() -> void:
 	if player.player_direction == Vector2.UP:
+		hit_component_collision_shape.position = Vector2(0, -18)
 		player_animated_sprite_2d.play("chopping_back")
 	elif player.player_direction == Vector2.RIGHT:
+		hit_component_collision_shape.position = Vector2(9, 0)
 		player_animated_sprite_2d.play("chopping_right")
 	elif player.player_direction == Vector2.LEFT:
 		player_animated_sprite_2d.play("chopping_left")
+		hit_component_collision_shape.position = Vector2(-9, 0)
 	elif player.player_direction == Vector2.DOWN:
+		hit_component_collision_shape.position = Vector2(0, 3)
 		player_animated_sprite_2d.play("chopping_front")
 	else:
 		player_animated_sprite_2d.play("chopping_front")
+		hit_component_collision_shape.position = Vector2(0, 3)
+		
+	hit_component_collision_shape.disabled = false
 
 func _on_exit() -> void:
 	player_animated_sprite_2d.stop()
+	hit_component_collision_shape.disabled = true
+	hit_component_collision_shape.position = Vector2(0, 0)
